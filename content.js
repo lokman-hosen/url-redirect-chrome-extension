f9b8ef91084df4517f5bf8a86c6d5761abbc02f0
lokman-hosen <lokman@mcc.com.bd>
2021-04-09 20:22:52 +0600

    first load, redirect link and UI design done

--- content.js
+++ content.js
@@ -2,7 +2,13 @@
 //alert(window.location.href);
 //window.location.replace("http://stackoverflow.com");
 var currentUrs = window.location.href;
-if ((currentUrs.indexOf("facebook") > -1) || (currentUrs.indexOf("google") > -1)) {
-    window.location.replace("http://youtube.com");
+if ((currentUrs.indexOf("facebook") > -1)) {
+    window.location.replace("https://www.youtube.com/watch?v=AjgD3CvWzS0");
+}else if ((currentUrs.indexOf("reddit") > -1)){
+    window.location.replace("https://www.youtube.com/watch?v=S2BfESvAKJA");
+}else if ((currentUrs.indexOf("porn") > -1)){
+    window.location.replace("https://www.youtube.com/watch?v=9yWMdCKCxow");
+}else if ((currentUrs.indexOf("Omegle") > -1)){
+    window.location.replace("https://www.youtube.com/watch?v=9yWMdCKCxow");
 }
 
